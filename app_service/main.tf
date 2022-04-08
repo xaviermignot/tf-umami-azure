7f2169b5fdb204f558edcf4cf6a1b4c0ce585e14
Xavier Mignot <xavier.mignot@floa.com>
2022-04-08 19:31:18 +0200

    feat: Deploy app using Docker

--- app_service/main.tf
+++ app_service/main.tf
@@ -13,11 +13,12 @@ resource "azurerm_linux_web_app" "app" {
   location            = var.location
   service_plan_id     = azurerm_service_plan.plan.id
 
-  app_settings = var.app_settings
+  app_settings = merge(var.app_settings, { "DOCKER_REGISTRY_SERVER_URL" = "https://ghcr.io" })
 
   site_config {
     application_stack {
-      node_version = "16-lts"
+      docker_image     = "mikecao/umami"
+      docker_image_tag = "mysql-latest"
     }
   }
 
@@ -30,11 +31,4 @@ resource "azurerm_role_assignment" "kv_app_secrets" {
   scope                = var.key_vault_id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_linux_web_app.app.identity.0.principal_id
-}
-
-resource "azurerm_app_service_source_control" "repo" {
-  app_id                 = azurerm_linux_web_app.app.id
-  repo_url               = "https://github.com/mikecao/umami"
-  branch                 = "master"
-  use_manual_integration = true
 }
